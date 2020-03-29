7cc000189e8ff41a6eb0135822ea0ca62beb4fb0
Simon Headley <headleysj@gmail.com>
2020-03-29 12:52:11 +0200

    Constructor unit tests

---
+++ src/100DoorsCodeKata.Tests/DoorOperatorTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+namespace HundredDoorsCodeKata.Tests
+{
+    [ExcludeFromCodeCoverage]
+    [TestFixture]
+    public class DoorOperatorTests
+    {
+        [Test]
+        public void DoorOperator_GivenOneDoor_ShouldCreateDoorOperatorWithOneDoor()
+        {
+            // Arrange
+            const int numOfDoors = 1;
+
+            // Act
+            var doorOperator = new DoorOperator(numOfDoors);
+
+            // Assert
+            Assert.AreEqual(numOfDoors, doorOperator.NumberOfDoors);
+        }
+
+        [Test]
+        public void DoorOperator_GivenMaxInt_ShouldCreateDoorWithMaxIntDoors()
+        {
+            // Arrange
+            const int numOfDoors = int.MaxValue;
+
+            // Act
+            var doorOperator = new DoorOperator(numOfDoors);
+
+            // Assert
+            Assert.AreEqual(numOfDoors, doorOperator.NumberOfDoors);
+        }
+
+        [TestCase(0)]
+        [TestCase(int.MinValue)]
+        [SuppressMessage("ReSharper", "ObjectCreationAsStatement")]
+        public void DoorOperator_GivenInvalidNumberOfDoors_ShouldThrowArgumentOutOfRangeException(int numOfDoors)
+        {
+            // Arrange
+            const string expectedParamName = "numOfDoors";
+
+            // Act
+            var actual = Assert.Throws<ArgumentOutOfRangeException>(() => new DoorOperator(numOfDoors));
+
+            // Assert
+            Assert.AreEqual(expectedParamName, actual.ParamName);
+        }
+    }
+}
---
+++ src/100DoorsCodeKata/DoorOperator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HundredDoorsCodeKata
+{
+    public class DoorOperator
+    {
+        public DoorOperator(int numOfDoors)
+        {
+            if (numOfDoors <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numOfDoors));
+            }
+
+            NumberOfDoors = numOfDoors;
+        }
+
+        public int NumberOfDoors { get; }
+    }
+}
